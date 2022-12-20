e709210cdb3025a1097c45158997363c06b0bf64
Mahdi Bozorgi <102772767+Mehtisup@users.noreply.github.com>
2022-12-20 15:05:28 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Show Password</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="theme-color" content="black">
+</head>
+<body>
+<div class="content">
+<label for="password">Password</label>
+<br>
+<br>
+<input type="password" id="myInput" placeholder="Password" value="1234">
+<button id="check" type="checkbox" onclick="showFunction()">Show</button>
+</div>
+</body>
+</html>
